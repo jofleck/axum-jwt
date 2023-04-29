254f817fe14ed8337412485afcd14882eec34ef7
Jonas Fleck <jonas@fleckmail.de>
2023-04-29 11:32:49 +0200

    Add realm roles (keycloak)

--- src/lib.rs
+++ src/lib.rs
@@ -156,9 +156,15 @@ pub struct Claims {
     pub sub: String,
     pub exp: usize,
     pub scope: String,
+    pub realm_access: Option<RealmAccess>,
     pub given_name: Option<String>,
     pub family_name: Option<String>,
     pub email: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RealmAccess {
+    pub roles: Vec<String>,
 }
 
 #[derive(Debug)]
